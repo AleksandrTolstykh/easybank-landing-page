468a32c18ed13d07af4e87c9167e5a94b8396b71
Aleksandr Tolstykh <tolstykh.alex@gmail.com>
2021-10-31 18:06:33 +0300

    Fix image paths

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -21,19 +21,19 @@ export class FeaturesComponent implements OnInit {
   @Input() columns = 0;
   features: Feature[] = [
     new Feature(
-      "../../assets/icon-online.svg",
+      "assets/icon-online.svg",
       "Online Banking",
       "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."),
     new Feature(
-      "../../assets/icon-budgeting.svg",
+      "assets/icon-budgeting.svg",
       "Simple Budgeting",
       "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."),
     new Feature(
-      "../../assets/icon-onboarding.svg",
+      "assets/icon-onboarding.svg",
       "Fast Onboarding",
       "We don’t do branches. Open your account in minutes online and start taking control of your finances right away."),
     new Feature(
-      "../../assets/icon-api.svg",
+      "assets/icon-api.svg",
       "Open API",
       "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."),
   ];
--- src/app/latest-articles/latest-articles.component.ts
+++ src/app/latest-articles/latest-articles.component.ts
@@ -23,22 +23,22 @@ export class LatestArticlesComponent implements OnInit {
   @Input() columns = 0;
   articles: Article[] = [
     new Article(
-      "../../assets/image-currency.jpg",
+      "assets/image-currency.jpg",
       "By Claire Robinson",
       "Receive money in any currency with no fees",
       "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …"),
     new Article(
-      "../../assets/image-restaurant.jpg",
+      "assets/image-restaurant.jpg",
       "By Wilson Hutton",
       "Treat yourself without worrying about money",
       "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …"),
     new Article(
-      "../../assets/image-plane.jpg",
+      "assets/image-plane.jpg",
       "By Wilson Hutton",
       "Take your Easybank card wherever you go",
       "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …"),
     new Article(
-      "../../assets/image-confetti.jpg",
+      "assets/image-confetti.jpg",
       "By Claire Robinson",
       "Our invite-only Beta accounts are now live!",
       "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ..."),
